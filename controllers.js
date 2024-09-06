0ed35cc97b877ab532b705f61c04c93a1fe80c58
Iryna S. <78602594+au-rinko@users.noreply.github.com>
2024-09-06 14:54:44 +0300

    feat: add functions of updating and deleting articles, adding comments

--- controllers.js
+++ controllers.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const { parseUrl, parseBodyJson } = require('./parsers');
 
-const SERVER_SUCESS = 200;
+const SERVER_SUCCESS = 200;
 
 const jsonData = fs.readFileSync('articles.json', 'utf8');
-const array = JSON.parse(jsonData);
+let array = JSON.parse(jsonData);
 
 function hello(req, res) {
     res.statusCode = SERVER_SUCESS;
@@ -15,13 +15,13 @@ function hello(req, res) {
 }
 
 function readAll(req, res) {
-    res.statusCode = 200;
+    res.statusCode = SERVER_SUCCESS;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.end(jsonData);
 }
 
 function read(req, res, params) {
-    res.statusCode = 200;
+    res.statusCode = SERVER_SUCCESS;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     const result = array.filter(item => item.id == params.id);
     if(result.length !== 0) {
@@ -41,10 +41,76 @@ function create(req, res) {
             return;
         }
         
-        res.statusCode = 200;
+        res.statusCode = SERVER_SUCCESS;
         res.setHeader('Content-Type', 'application/json');
         const obj = Object.assign({ id: array.length + 1}, result);
         array.push(obj);
+        writeToFile(array);
+        res.end( JSON.stringify( obj ) );
+    });
+}
+
+function update(req, res, params) {
+    parseBodyJson(req, (err, result) => {
+        if (err) {
+            res.statusCode = err.code;
+            res.setHeader('Content-Type', 'application/json');
+            res.end( JSON.stringify(err) );
+    
+            return;
+        }
+        
+        res.statusCode = SERVER_SUCCESS;
+        res.setHeader('Content-Type', 'application/json');
+        console.log(params.id);
+        array = array.map(item => item.id !== +params.id ? item: {
+                    id: item.id,
+                    title: result.title || item.title,
+                    text: result.text || item.text,
+                    date: result.date || item.date,
+                    author: result.author || item.author,
+                    comments: item.comments
+            }
+        );
+        writeToFile(array);
+        res.end( 'Updated' );
+    });
+}
+
+function deleteArt(req, res, params) {
+    res.statusCode = SERVER_SUCCESS;
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    console.log(params.id);
+    if(+params.id > array.length){
+        res.end(`There is no such article`);
+    } else {
+        array = array.filter(item => item.id !== +params.id).map((item, index) => item.id = {
+            id: index + 1,
+            title: item.title,
+            text: item.text,
+            date: item.date,
+            author: item.author,
+            comments: item.comments
+        });
+        writeToFile(array);
+        res.end('Deleted');
+    }
+}
+
+function createComment(req, res, params) {
+    parseBodyJson(req, (err, result) => {
+        if (err) {
+            res.statusCode = err.code;
+            res.setHeader('Content-Type', 'application/json');
+            res.end( JSON.stringify(err) );
+    
+            return;
+        }
+        
+        res.statusCode = SERVER_SUCCESS;
+        res.setHeader('Content-Type', 'application/json');
+        const obj = Object.assign({ id: array[+params.id - 1].comments.length + 1}, {articleId: params.id}, result);
+        array[+params.id - 1].comments.push(obj);
         writeToFile(array);
         res.end( JSON.stringify( obj ) );
     });
@@ -66,5 +132,8 @@ module.exports = {
     readAll,
     read,
     create,
+    update,
+    deleteArt,
+    createComment,
     notFound
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const { parseUrl, parseBodyJson } = require('./parsers');
-const { hello, readAll, read, create, notFound } = require('./controllers');
+const { hello, readAll, read, create, update, deleteArt, createComment, notFound } = require('./controllers');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -10,7 +10,10 @@ const handlers = {
     '/': hello,
     '/articles/readall': readAll,
     '/articles/read': read,
-    '/articles/create': create
+    '/articles/create': create,
+    '/articles/update': update,
+    '/articles/delete': deleteArt,
+    '/comments/create': createComment,
 };
 
 const server = http.createServer(handler);
